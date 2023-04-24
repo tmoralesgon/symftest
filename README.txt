c542508071bff0c526590ddb653b6f9396ef5df4
Tatiana Morales <tatiana.morales.gonzalez@gmail.com>
2023-04-24 12:50:49 +0200

    Bootstrap has been added

--- README.txt
+++ README.txt
@@ -1,4 +1,17 @@
-http://localhost:8000/main
+symfony server:start
 
-php bin/console debug:router
+php bin/console debug:router (ejemplo: http://localhost:8000/main)
 
+# Pasos para installar Doctrine en Symfony 6 y + # 
+
+composer require "doctrine:^2.2" --with-all-dependencies
+
+composer require --dev symfony/maker-bundle
+
+php bin/console docrtine:database:create (siempre que se tenga la línea de la bdd en .env definida)
+
+php bin/console make:entity
+
+php bin/console make:migration
+
+php bin/console doctrine:migrations:migrate
--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -1,16 +1,25 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Item;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ItemController extends AbstractController
 {
-    public function show(): Response
+    public function show(EntityManagerInterface $em): Response
     {
-        $userFirstName = "Tatiana";
+        $allItems = $em->getRepository(Item::class)->findAll();
+        $message="";
+        if (count($allItems)<1) {
+            /*throw $this->createNotFoundException(
+                'No items found'
+            );*/
+            $message="No items found";
+        }
 
-        return $this->render('item/items.html.twig', ['user_name' => $userFirstName]);
+        return $this->render('item/items.html.twig', ['items' => $allItems, 'message' => $message]);
     }
 }
